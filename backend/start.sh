3c9cc88827d33c7a9d153fc7ad21008679db666b
Jake Martin <jm9357481@gmail.com>
2022-11-28 20:13:43 -0800

    remove unused

--- backend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-envsubst < /app/src/backend.conf.temp > /app/src/backend.conf
-
-/app/src/wait-for-it.sh database:3306
-exec /app/.local/bin/gunicorn wsgi:app -b 0.0.0.0:8000
